ea53010eee44fc1662e2fd44e4f1543c12ea34fa
Madhu <madhuedipelli@outlook.com>
2023-01-08 18:45:24 +0530

    take args

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # gradebook
 C# Fundamentals
+
+1.Introduction -- done
+2.C# syntax -- in progress
+
+Tough and Essential part :
+	Understanding user requirements and
+	coming up with good system design to solve
+	the user defined problem
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length>0)
+            {
+                Console.WriteLine($"Hello,{args[0]}");
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
+        }
+    }
+}
